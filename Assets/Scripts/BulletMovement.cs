78c58649ae71359dcfc9d3b396ec183fe58cedcc
DeepikaVasam123 <deepika.vasam@xcubelabs.com>
2021-06-24 07:56:31 -0700

    Player movement, Enemy soawn through object pooling, UI is done

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(velocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
